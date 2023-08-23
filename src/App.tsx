08b012667b64faa0c3bbe69f8797e60c43af2b87
Khanim3 <139584395+Khanim8@users.noreply.github.com>
2023-08-23 17:15:43 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View} from "react-native";
-import Girl from './assets/icon/Girl.svg';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import Girl from './assets/image/Girl.svg'
 
 export const App = () => {
     return (
         <SafeAreaView style={styles.safeArea}>    
               <View>
-              <Girl/>
+                <Girl/>
               </View>
               <View>
-                <Text style={styles.title}>Make your favorites recipes</Text>
+              <Text style={styles.title}>Make your favorites recipes</Text>
                 <Text style={styles.about}>Just tell us what ingredients do you have and we will show you
-what can you make.</Text>
+                what can you make.</Text>
+              </View>
+              <View style={styles.button}>
+                <TouchableOpacity>
+                    <Text style={styles.text}>Getstarted</Text>
+                </TouchableOpacity>
               </View>
        </SafeAreaView>
     );           
@@ -28,7 +33,7 @@ const styles = StyleSheet.create( {
      color: '#FFF',
      textAlign: 'center',
      fontSize: 45,
-     fontweight: 700,
+     fontweight: '700',
     },
     about: {
         width: 298,
@@ -36,7 +41,19 @@ const styles = StyleSheet.create( {
         color: '#FFF',
         textAlign: 'center',
         fontSize: 18,
-        fontweight: 400,
+        fontweight: '400',
     },
+    button: {
+        width: 314,
+        height: 70,
+        borderRadius: 30,
+        backgroundColor: '#272D2F',
+    },
+    text: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 19,
+        fontweight: '700',
+        lineheight: 'normal',
+    }
 });
-
