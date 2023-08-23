7b8ec1de942333a588ba181bc6119bba9fc5513c
Fidan Əlizadə <139375113+Fidanelizade@users.noreply.github.com>
2023-08-23 11:23:10 +0300

    searchbar etmek istedim ancaq duzgun alinmadi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {Result} from './screens/resultPage'
+import {ResultPage} from './screens/resultPage'
+import { NavigationContainer } from '@react-navigation/native';
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <StackNavigator>
-      <Stack.Screen name="Result" componenet={Result}/>
-    </StackNavigator>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="ResultPage" component={ResultPage}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 };
--- src/screens/resultPage/index.tsx
+++ src/screens/resultPage/index.tsx
@@ -1,43 +1,82 @@
-import React from 'react';
-import  HeartIcon from '../../assets/icons/heart.svg';
-import { View, StyleSheet } from 'react-native';
+import React  from 'react';
+import HeartIcon from '../../assets/icons/heart.svg';
+import {View, StyleSheet, Text,} from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { TextInput } from 'react-native-gesture-handler';
+
 
 export const Result = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <HeartIcon />
+      </View>
+      <View style={styles.greeting}>
+        <Text style={styles.greetingText}>Here's what{'\n'}you can make</Text>
+      </View>
+      <View style={styles.search}>
+        <TextInput
+        style={{ padding: 10, borderBottomWidth: 1 }}
+        placeholder="Search..."
+        value={searchQuery}
+        onChangeText={handleSearch}
+      />
+      </View>
+      <View style={styles.result} />
+    </View>
+  );
+};
+
+export default class App extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
     return (
-        <View style={styles.container}/>
-        <View style={styles.header}>
-            <HeartIcon/>
-        </View>
-        <View style={styles.greetings}/>
-        <View style={styles.search}/>
-        <View style={styles.category}/>
-        <View style={styles.result}/>
-        
-    )
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+    );
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#FFFFFF',
-    },
-    header: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
-    greetings: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
-    search: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
-    category: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    },
-    result: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  header: {
+    paddingHorizontal: 25,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  greeting: {
+    marginTop: 40,
+    paddingHorizontal: 25,
+    lineHeight: 30,
+  },
+  greetingText: {
+    color: '#272D2F',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  search: {
+    color:'#E6E6E6',
+    paddingTop: 242,
+    paddingRight: 50,
+    paddingLeft: 50,
+  },
+  
+
+  result: {
+    flex: 1,
+  },
+});
