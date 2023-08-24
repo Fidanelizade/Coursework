042f38ab5e2af5b8189a3a7992e954e2a19b2381
Fidan Əlizadə <139375113+Fidanelizade@users.noreply.github.com>
2023-08-24 11:14:51 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,11 +1,61 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import HeartIcon from '../../assets/icons/heart.svg';
-import {View, StyleSheet, Text,} from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import { TextInput } from 'react-native-gesture-handler';
+import {View, StyleSheet, Text, TouchableOpacity,TextInput} from 'react-native';
+
+const categories=[
+  {
+    title: 'Name',
+    key: 'name'
+  },
+  {
+    title: 'Category',
+    key: 'category'
+  },
+  {
+    title: 'Area',
+    key: 'area'
+  },
+  {
+    title: 'Ingredient',
+    key: 'ingredient'
+  }
+]
+
+const SearchBar = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState('');
+const  [selectedCategory,setSelectedCategory] = useState('name')
+  const handleSearch = () => {
+    onSearch(searchText);
+  };
+
+  return (
+    <View style={styles.search}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search..."
+        value={searchText}
+        onChangeText={setSearchText}
+        onSubmitEditing={handleSearch}
+      />
+      <View>
+        {categories.map(item=>{
+          return (
+       <TouchableOpacity style={{backgroundColor: selectedCategory === item.key ? "tomato" : "transparent"}}
+       onPress={() => setSelectedCategory(item.key)}>
+        <Text>
+          {item.title}
+        </Text>
+        </TouchableOpacity>
+          )
+        })}
+      </View>
+    </View>
+  );
+};
 
 
-export const Result = () => {
+
+export const ResultPage = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -14,40 +64,13 @@ export const Result = () => {
       <View style={styles.greeting}>
         <Text style={styles.greetingText}>Here's what{'\n'}you can make</Text>
       </View>
-      <View style={styles.search}>
-        <TextInput
-        style={{ padding: 10, borderBottomWidth: 1 }}
-        placeholder="Search..."
-        value={searchQuery}
-        onChangeText={handleSearch}
-      />
-      </View>
-      <View style={styles.result} />
+      <SearchBar/>
     </View>
   );
 };
 
-export default class App extends React.Component {
-  state = {
-    search: '',
-  };
+  
 
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
-
-  render() {
-    const { search } = this.state;
-
-    return (
-      <SearchBar
-        placeholder="Type Here..."
-        onChangeText={this.updateSearch}
-        value={search}
-      />
-    );
-  }
-}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -69,14 +92,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   search: {
-    color:'#E6E6E6',
-    paddingTop: 242,
-    paddingRight: 50,
     paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 242,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#E6E6E6',
+    borderRadius: 20,
   },
   
-
-  result: {
-    flex: 1,
-  },
 });
